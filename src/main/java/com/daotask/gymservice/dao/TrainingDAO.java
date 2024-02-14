ea2cfb4f46bb8293d7da07d64107372a7e919163
Jorge Canto <jorgercduran02@gmail.com>
2024-02-14 18:32:40 -0500

    Changes not comitted

--- src/main/java/com/daotask/gymservice/dao/TrainingDAO.java
+++
@@ -1,7 +0,0 @@
-package com.daotask.gymservice.dao;
-
-import com.daotask.gymservice.entities.Training;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainingDAO extends JpaRepository<Training, Long> {
-}
