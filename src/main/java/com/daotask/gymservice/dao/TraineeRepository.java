d445ff43256aa8f79f8ffc9c71b39a09f997b0a4
Jorge Canto <jorgercduran02@gmail.com>
2024-02-14 23:06:31 -0500

    Changes before adding services

---
+++ src/main/java/com/daotask/gymservice/dao/TraineeRepository.java
@@ -0,0 +1,12 @@
+package com.daotask.gymservice.dao;
+
+import com.daotask.gymservice.entities.Trainee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TraineeRepository extends JpaRepository<Trainee,Long> {
+    Optional<Trainee> findByUsername(String username);
+}
